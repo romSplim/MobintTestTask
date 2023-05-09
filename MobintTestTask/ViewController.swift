d039d1d9891b310bc7cfdf2392ef361198193131
ramil <ramil@MacBook-Air-ramil.local>
2023-05-09 14:00:43 +0700

    initial commit

---
+++ MobintTestTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MobintTestTask
+//
+//  Created by ramil on 09.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
